59c9760eb8c2c9a83f702723f6e265bb0167a859
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2017-10-26 02:51:45 +0200

    O ovom commitu napravite su 3 stvari. 1) Dodan je RecyclerView sa implementacijom adaptera koji zasada radi sa tiles elementima liste vozača, tj. dobiva listu objekata te zatim pomoću RecyclerView-a, adaptera obrađuje određeni skup podataka. Testirano sa testnim podacima. Potrebno je još prilagoditi neke stvari za prave objekte koji će se prosljeđivati adapteru. Također ne bi bilo loše dodatno poraditi na dizajnu tilesa. 2) Na login activity-u je dodan gumb za testiranje kako bi mogli testirati određene dijelove aplikacije s obzirom da login još nije gotov. 3) Uspostavljena je veza između glavnog ekrana i ekrana koji prikazuje vozače u obliku tilesa. Taj ekran je još potrebno prilagoditi, odnosno potrebno je napraviti takav layout koji će moći na jednostavan način mijenjati načine prikaza vozača.

--- app/src/main/java/hr/foi/air/trucktrack/Drivers.java
+++ app/src/main/java/hr/foi/air/trucktrack/Drivers.java
@@ -2,12 +2,48 @@ package hr.foi.air.trucktrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.foi.air.drivermodule.DriversAdapter;
 
 public class Drivers extends AppCompatActivity {
+
+    ArrayList<String> a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drivers);
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvDrivers);
+
+        // Initialize contacts
+        a = new ArrayList<>();
+        a.add("Ivan");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        a.add("Pero");
+        // Create adapter passing in the sample user data
+        DriversAdapter adapter = new DriversAdapter(this, a);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        // That's all!
     }
 }
