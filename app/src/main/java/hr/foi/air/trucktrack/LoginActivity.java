080b2b953be8aaa1b22f1fd8d497c9575951bbd4
Nikola Fluks <flukscro@gmail.com>
2017-11-01 22:27:12 +0100

    Popravljen login activity.

--- app/src/main/java/hr/foi/air/trucktrack/LoginActivity.java
+++ app/src/main/java/hr/foi/air/trucktrack/LoginActivity.java
@@ -1,52 +1,39 @@
 package hr.foi.air.trucktrack;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-import static hr.foi.air.trucktrack.R.id.login;
+import android.widget.Toast;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
 
+    EditText user, password;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(getApplicationContext(), DisponentHome.class));
+        setContentView(R.layout.activity_login);
 
+        user = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        Button signInButton = (Button)findViewById(R.id.loginButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (user.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Preusmjeravanje...", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Pogrešan unos!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
 
