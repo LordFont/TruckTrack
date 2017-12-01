52d01557c9108cbdcf690f35d528573b4f0005bb
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2017-12-01 03:45:11 +0100

    Prikaz neprihvaćene rute i trenutno aktivne na vrhu liste(2. commit) Napravito je sljedeće: 1. U activity-u DriverJobs dodani su testni podaci te prikazivanje fragmenta unutar activity-a koji prikazuje poslove vozaču u svrhu testiranja aplikacije. To je implementirano tako što smo kreirali testnu listu sa podacima koju prosljeđujemo funkciji showFragment() koja postavlja fragment unutar containera. 2. Refaktorizirani su modeli za rutu i posao (JobModel, RuoteModel) kako bi aplikacija trenutno radila bez servera u svrhu testiranja. Promijenit je tip podata za datume utovara i datume istovara te su dodani getteri i setteri. 3. Dodana je klasa, odnosno fragment DriverJobsFragment koji se veže na activity DriverJobs. Taj fragment je potreban kako bi se podaci ispisivali vozaču u odgovarajućem obliku. Zajedno sa tom klasom dolazi layout fragment_driver_jobs čiji je element recyclerview. U klasi DriverJobsFragment uz implementaciju konstruktora i pridruživanja odgovarajućih podataka (dataJobs) prikazujemo te podatke pomoću RecyclerView-a. Za prikazivanje koristimo metodu showRecycleView. 4. Ova točka se odnosi na konkretnu implementaciju RecycleView-a i nadovezuje se na točku 3. U ovom slučaju riječ je o expandable RecyclerView-u što znači da sadrži parent item i child item te adapter koji povezuje te iteme u jednu cjelinu (DriverJobsAdapter). Pripadajući početni dizajn itema nalazi se u datotekama child_driver_job.xml i parent_driver_job.xml. Već spomenuti adapter preko metode onBindViewHolder s obzirom na odgovarajuću vrstu ViewHoldera (Parent ili Child) postavlja podatke na pripadajuće mjesto (tako što se dohvati preko id-a iz xml-a to mjesto i zatim se preko setText postavi podatak o ruti (parent), poslu(Child). Preko if...else strukture utvrđujemo o kojoj vrsti ViewHoldera je riječ.

--- app/src/main/java/hr/foi/air/trucktrack/DriverJobs.java
+++ app/src/main/java/hr/foi/air/trucktrack/DriverJobs.java
@@ -1,24 +1,48 @@
 package hr.foi.air.trucktrack;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
-public class DriverJobs extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import entities.RouteModel;
+import hr.foi.air.drivermodule.ListViewFragment;
+
+public class DriverJobs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_jobs);
 
         initToolbar();
+
+        //kreiranje dummy podataka u svrhu testiranja bez servera
+        RouteModel routeJob1 = new RouteModel();
+        RouteModel routeJob2 = new RouteModel();
+        routeJob1.CreateTestData();
+        routeJob2.CreateTestData();
+        ArrayList<RouteModel> testList = new ArrayList<>();
+        testList.add(routeJob1);
+        testList.add(routeJob2);
+
+        showFragment(DriverJobsFragment.getInstance(testList));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_standard, menu);
         return true;
+    }
+
+    private void showFragment(Fragment f) {
+        FragmentTransaction mTransactiont = getSupportFragmentManager().beginTransaction();
+        mTransactiont.replace(R.id.main_container, f, f.getClass().getName());
+        mTransactiont.commit();
     }
 
     public void initToolbar() {
