de8fe3b74361f0408e819d8dce4c4ed58f60dfd3
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2017-12-05 17:40:38 +0100

    Fixing Problems - Povezivanje fragmenta liste vozača sa fragmentom dodavanja novoga posla Dodali smo apstraktnu klasu FragmentManager koji nasljeđuje svaki callback (zasada samo UserCallback) sa kojim radimo tako da se može izvršiti tranzicija na bilo kojem fragmentu kojega želimo kada dohvatimo podatke sa servera. Razlog primjene ovakvoga pristupa je pokušamo odvojiti Callback metode od ponavljanja koda u aktivnostima gdje je ta metoda potrebna na više mjesta, a ujedno omogućimo modularnost naših fragmenata.

---
+++ app/src/main/java/hr/foi/air/trucktrack/Helpers/FragmentManager.java
@@ -0,0 +1,23 @@
+package hr.foi.air.trucktrack.Helpers;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+
+import hr.foi.air.trucktrack.R;
+
+/**
+ * Created by Ivan on 5.12.2017..
+ */
+
+public abstract class FragmentManager {
+    protected FragmentActivity mCurrent;
+    protected Fragment mNnext;
+    protected void showFragment() {
+        FragmentTransaction mTransactiont = mCurrent.getSupportFragmentManager().beginTransaction();
+        mTransactiont.replace(R.id.main_container, mNnext, mNnext.getClass().getName());
+        mTransactiont.commit();
+    }
+}
