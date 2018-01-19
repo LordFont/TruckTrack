87e7f4d180612793586c313f5345d1ec3372c5bc
ss <ssajcic@tria.hr>
2018-01-19 22:34:27 +0100

    popravljen UX u listi poslova disponenta i vozača

--- app/src/main/java/hr/foi/air/trucktrack/Adapters/JobListAdapter.java
+++ app/src/main/java/hr/foi/air/trucktrack/Adapters/JobListAdapter.java
@@ -1,5 +1,8 @@
 package hr.foi.air.trucktrack.Adapters;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -7,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +37,7 @@ public class JobListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     final int DIALOG_SAVE_JOB = 200;
     final int DIALOG_SET_DONE = 400;
     final int DIALOG_ACK_TO_JOB = 500;
-
+    final int JOB_ACK = 1;
 
     public JobListAdapter(ArrayList<Object> data, String tip, Fragment context, CustomDialog customDialog) {
         dataOfTheList = data;
@@ -78,12 +83,22 @@ public class JobListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
                 }
             }
 
-            String datum1 = "Nema ga";
             String datum2 = ((RouteModel) dataOfTheList.get(position)).getIstovarDatum().toString();
-            ((TextView) parent.findViewById(R.id.datumi)).setText("Datum rute: " + datum2);
+            ((TextView) parent.findViewById(R.id.datumi)).setText(datum2);
+            ((TextView) parent.findViewById(R.id.datumi)).setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
             ((TextView) parent.findViewById(R.id.utovar)).setText(((RouteModel) dataOfTheList.get(position)).getMjestoUtovara().toString());
             ((TextView) parent.findViewById(R.id.istovar)).setText(((RouteModel) dataOfTheList.get(position)).getMjestoIstovara());
             ((TextView) parent.findViewById(R.id.status)).setText(((RouteModel) dataOfTheList.get(position)).getStatus().toString());
+
+             if (((RouteModel) dataOfTheList.get(position)).getStatusId() == JOB_ACK) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    parent.setBackgroundColor(contextAct.getResources().getColor(R.color.colorACK, contextAct.getContext().getTheme()));
+                } else {
+                    parent.setBackgroundColor(contextAct.getResources().getColor(R.color.colorACK));
+                }
+
+                setColorWhite((ViewGroup) parent);
+            }
 
             parent.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -170,6 +185,28 @@ public class JobListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     public void removeFrom(int position, int numOfJobs) {
         for (int i = 0; i < numOfJobs; i++)
             dataOfTheList.remove(position + 1);
+    }
+
+    public void setColorWhite(ViewGroup view) {
+        for (int i = 0; i < view.getChildCount(); i++) {
+            View linear = view.getChildAt(i);
+            for (int j = 0; j < ((ViewGroup) linear).getChildCount(); j++) {
+                View child = ((ViewGroup) linear).getChildAt(j);
+                if (child instanceof TextView) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        ((TextView) child).setTextColor(contextAct.getResources().getColor(R.color.textbody, contextAct.getContext().getTheme()));
+                    } else {
+                        ((TextView) child).setTextColor(contextAct.getResources().getColor(R.color.textbody));
+                    }
+                } else if (child instanceof ImageView) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        ((ImageView) child).setColorFilter(contextAct.getResources().getColor(R.color.textbody, contextAct.getContext().getTheme()));
+                    } else {
+                        ((ImageView) child).setColorFilter(contextAct.getResources().getColor(R.color.textbody));
+                    }
+                }
+            }
+        }
     }
 }
 
