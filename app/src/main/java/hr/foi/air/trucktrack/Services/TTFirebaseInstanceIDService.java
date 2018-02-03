b530d5c99cd4f3dcc878f455e2cb76cc1ec93884
Roman Tomašković <romantomask@gmail.com>
2018-02-03 17:29:38 +0100

    Dodana podrška za firebase messaging (push notifikacije)

---
+++ app/src/main/java/hr/foi/air/trucktrack/Services/TTFirebaseInstanceIDService.java
@@ -0,0 +1,40 @@
+package hr.foi.air.trucktrack.Services;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by roman on 1/31/18.
+ */
+
+public class TTFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String FCM_TOKEN = "FCM_TOKEN";
+    private static final String TAG = "TRUCKTRACK_FCM_TOKEN";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        saveToken(refreshedToken);
+    }
+
+    private void saveToken(String token)
+    {
+        //save to shared preferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.edit().putString(FCM_TOKEN, token).apply();
+
+        //send to your own web service
+        //TODO
+
+    }
+}
